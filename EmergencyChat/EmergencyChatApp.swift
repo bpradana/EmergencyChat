a20304ab00345e4c1d70918e5a40b10c68131ead
Bintang Pradana Erlangga Putra <bintang.pradana@privyid.tech>
2024-01-15 16:45:48 +0700

    Initial Commit

---
+++ EmergencyChat/EmergencyChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmergencyChatApp.swift
+//  EmergencyChat
+//
+//  Created by Bintang Pradana on 15/01/24.
+//
+
+import SwiftUI
+
+@main
+struct EmergencyChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
